9ade38c8a43fbe689def1d61e921219c267bd738
Chung Jong In <107922938+dddingzong@users.noreply.github.com>
2026-04-02 23:59:16 +0900

    [Silver III] Title: 바이러스, Time: 188 ms, Memory: 18192 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,50 +1,62 @@
 
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
 
 public class Main {
-
-    static boolean[] check;
-    static int count = 0;
-
-    static int[][] computers;
-    static int node;
-    static int line;
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-        node = input.nextInt();
-        line = input.nextInt();
-
-        check = new boolean[node+1];
-
-        computers = new int[node+1][node+1];
-
-        for (int i = 0;  i < line; i++) {
-            int nodeOne = input.nextInt();
-            int nodeTwo = input.nextInt();
-
-            computers[nodeOne][nodeTwo] = computers[nodeTwo][nodeOne] = 1;
-        }
-
-        dfs(1);
-
-        System.out.println(count - 1);
-
+	
+	static int n;
+	static List<List<Integer>> list;
+	static int cnt = 0;
+	static boolean[] visited;
+	
+    public static void main(String[] args) throws IOException{
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int k = sc.nextInt();
+    	int n = sc.nextInt();
+    	
+    	list = new ArrayList<>();
+    	visited = new boolean[k];
+    	
+    	for (int i=0;i<k;i++) {
+    		list.add(new ArrayList<>());
+    	}
+    	
+    	cnt = 0;
+    	
+    	for (int i=0;i<n;i++) {
+    		int number = sc.nextInt();
+    		int oppositenNumber = sc.nextInt();
+    		list.get(number-1).add(oppositenNumber);
+    		list.get(oppositenNumber-1).add(number);
+    	}
+    	
+    	visited[0] = true;
+    	int result = dfs(1);
+    	
+    	System.out.print(result);
+    	
+    	
+    	
     }
-
-    static void dfs(int start) {
-
-        check[start] = true;
-        count++;
-
-        for (int i = 0; i <= node; i++) {
-            if (computers[start][i] == 1 && !check[i]) {
-                dfs(i);
-            }
-        }
+    
+    
+    static int dfs(int start) {
+    	
+    	for (int i=0;i < list.get(start-1).size() ;i++) {
+    		int number = list.get(start-1).get(i);
+    		
+        	if (visited[number-1] == false) {
+        		visited[number-1] = true;
+        		cnt++;
+        		dfs(number);
+        	}
+    		
+    	}
+    	
+    	return cnt;
     }
+    
+    
 }
-
