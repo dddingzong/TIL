c151ea710d285b6c6723b91f4fcb4bdba6413d56
Chung Jong In <107922938+dddingzong@users.noreply.github.com>
2026-04-08 20:46:32 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,33 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
-        int sum = (brown+4)/2;
-        int total = brown + yellow;
+        // yellow = (x-2)*(y-2)
+        // yellow = xy -2(x+y) +4
+        // yellow = xy -2(brown/2+2) + 4
+        // yellow = xy -brown
+        // yellow + brown = xy
+        
+        // brown/2 + 2 = x+y
+        
+        int sum = brown/2 + 2;
+        int mul = brown + yellow;
+        
         int x=0;
         int y=0;
-        
-        for (x=sum-1, y=1; x >= y; x--, y++) {
-            if (x*y==total) {
-                answer = new int[]{x,y};
+        for (int i=3;i<sum;i++){
+            y = i;
+            x = sum-i;
+            
+            if (x+y == sum && x*y == mul) {
                 break;
             }
-        }
+            
+        } 
+        answer = new int[]{x,y};
+
+        
+        
+        
         
         return answer;
     }
