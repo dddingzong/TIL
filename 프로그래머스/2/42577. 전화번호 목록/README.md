a74aec7a2066167b12f537008ca6265afe41b4ac
Chung Jong In <107922938+dddingzong@users.noreply.github.com>
2026-05-27 19:06:45 +0900

    [level 2] Title: 전화번호 목록, Time: 174.68 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?utm_source=chatgpt.com) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 99 MB, 시간: 443.85 ms
+메모리: 104 MB, 시간: 174.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 08일 01:26:59
+2026년 05월 27일 19:06:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,17 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;        
         Arrays.sort(phone_book);
         
+        // for (String phone : phone_book) {
+        //     System.out.println(phone);
+        // }
+        
         for (int i=0;i<phone_book.length-1;i++) {
-            String cur = phone_book[i];
-            if (phone_book[i+1].startsWith(cur)){
-                answer = false;
-                break;
+            String phone = phone_book[i];
+            String nextPhone = phone_book[i+1];
+            if (nextPhone.startsWith(phone)) {
+                return false;
             }
         }
         
-        return answer;
+        return true;
     }
 }
