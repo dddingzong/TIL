77d2552c83c947f01572536bc51f72f06f964aa1
Chung Jong In <107922938+dddingzong@users.noreply.github.com>
2026-05-28 23:45:58 +0900

    [level 2] Title: 기능개발, Time: 0.85 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,47 +2,48 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> tmpAnswer = new ArrayList<>();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
         int n = progresses.length;
-        int[] answer = {};
-        int cycleCount = 0;
-
+        int next = 0;
+        boolean[] visited = new boolean[n];
+        List<Integer> answerList = new ArrayList<>();
         
-        Queue<Integer> que = new LinkedList<>();
-        
-        for (int i=0;i<n;i++) {
-            que.add(i);
-        }
-        
-        while (cycleCount != n){
-            int cnt =0;
-            
-            for (int i=0; i < n; i++) {
-                progresses[i] += speeds[i];
-                
-                if (progresses[i] >= 100) {
-                    int cur = que.peek();
-                    if (cur == i) {
-                        cnt++;
-                        que.poll();
-                    }
+        while (next != n) {
+            // 제거부터 진행
+            if (!pq.isEmpty() && pq.peek() == next) {
+                int cnt = 0;
+                while(!pq.isEmpty()){
+                    if (pq.peek() != next) break;
+                    int cur = pq.poll();
+                    cnt++;
+                    next++;
                 }
-                
+                answerList.add(cnt);
             }
             
-            if (cnt !=0) {
-                tmpAnswer.add(cnt);
+            for (int i=0;i<n;i++) { 
+                progresses[i] += speeds[i];
             }
             
-            cycleCount += cnt;
+            
+            // 100이 있는지 확인
+            for (int i=0;i<n;i++) {
+                if (progresses[i] >= 100 && visited[i] == false) {
+                    pq.add(i);
+                    visited[i] = true;
+                }
+            }
+            
+            
         }
         
-        answer = new int[tmpAnswer.size()];
-          
-        for (int i=0;i<tmpAnswer.size();i++) {
-            answer[i] = tmpAnswer.get(i);
+        int[] answer = new int[answerList.size()];
+        
+        for (int i=0;i<answer.length;i++) {
+            answer[i] = answerList.get(i);
         }
-         
+        
         return answer;
     }
 }
