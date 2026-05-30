4fca8b7d48d1277b55a3d8df9fe07409ca6bcc29
Chung Jong In <107922938+dddingzong@users.noreply.github.com>
2026-05-30 17:49:59 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,31 @@
 import java.util.*;
 
 class Solution {
+    static boolean[] visited;
     
-    static Queue<Integer> que;
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        // bfs
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
         
         for (int i=0;i<n;i++) {
-            if (visited[i]) continue;
-            
-            que = new LinkedList<>();
-            que.add(i);
-            visited[i] = true;
-            
-            while (!que.isEmpty()) {
-                int curr = que.poll();
-                
-                int[] injectList = computers[curr];
-                for (int j=0;j<n;j++) {
-                    if (injectList[j]==0 || visited[j]) continue;
-                    que.add(j);
-                    visited[j] = true;
-                    
-                }   
-            }  
+            if (visited[i] == true) continue;
+            dfs(i,computers);
             answer++;
-            
-        }        
-        
+        }
         
         return answer;
     }
+    
+    static void dfs(int i, int[][] computers) {
+        visited[i] = true;
+        
+        int[] cur = computers[i];
+        for(int j=0;j<cur.length;j++) {
+            if (visited[j] == true || cur[j] == 0) continue;
+            dfs(j,computers);
+        }
+        
+    }
+    
 }
